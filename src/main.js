2cfd62d5121c6f4655337fc5e84bc00b543b7a60
753495441 <753495441@qq.com>
2019-10-15 18:29:47 +0800

    123

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import Vant from 'vant';
-import 'assets/index.css';
+import 'css/index.css';
 
 import { Toast } from 'vant';
 import { Area } from 'vant';
 import { Lazyload } from 'vant';
 import { Dialog } from 'vant';
-import '@/assets/index.css';
+import '@/css/index.css';
 
 // options 为可选参数，无则不传
 Vue.use(Lazyload);
